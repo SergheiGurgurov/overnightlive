e46e134d3cde0a61b063f985089635838b0f1fbb
Serghei Gurgurov <serghei.gurgurov@gmail.com>
2025-03-16 21:06:43 +0100

    updated channels

--- src/chat.js
+++ src/chat.js
@@ -13,6 +13,15 @@ function init(channels,onMessage) {
     client.connect();
     
     client.on('message', onMessage);
+    client.on('connected', () => {
+        console.log('Connected to IRC');
+    });
+    client.on('disconnected', () => {
+        console.log('Disconnected from IRC');
+    });
+    client.on('error', (err) => {
+        console.log('TMI Error:', err);
+    });
 }
 
 exports.init = init;
